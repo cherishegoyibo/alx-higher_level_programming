d1e772109b666774e9d365b78bf599db48bb10ea
cherishegoyibo <cherishegoyibo@gmail.com>
2023-10-05 09:19:17 -0700

    fix task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
 
     arg_count = len(sys.argv) - 1
-    if count == 0:
+    if arg_count == 0:
         print("0 arguments.")
     elif arg_count == 1:
         print("1 argument:")
