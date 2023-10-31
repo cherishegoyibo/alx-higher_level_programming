e497de09372c8d9622eba8b188d97c05487bf9f0
cherishegoyibo <cherishegoyibo@gmail.com>
2023-10-30 21:01:17 -0700

    mandatory task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+A class that defines a Rectangle
+"""
+
+
+class Rectangle:
+    """Empty class representing a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+# 0x08. Python - More Classes and Objects
+This directory contains task solution for
+project 0x08. Python - More Classes and Objects
