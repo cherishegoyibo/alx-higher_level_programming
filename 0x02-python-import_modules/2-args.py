2aaae132eb8550fe7cfa52820f4b627a97c0127b
cherishegoyibo <cherishegoyibo@gmail.com>
2024-03-15 02:06:00 +0100

    mandatory task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    numb_arg = len(sys.argv) - 1
+    if numb_arg == 0:
+        print("0 arguments.")
+    elif numb_arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(numb_arg))
+    for i in range(numb_arg):
+        print("{}: {}".format(i +1, sys.argv[i + 1]))
