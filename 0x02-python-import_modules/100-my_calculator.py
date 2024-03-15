9517987594f5fc605fac19cc7ea807eb73d46272
cherishegoyibo <cherishegoyibo@gmail.com>
2024-03-15 05:57:19 +0100

    advance

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
     from calculator_1 import add, sub, mul, div
+    import sys
 
     if len(sys.agrv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
